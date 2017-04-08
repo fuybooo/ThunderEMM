37bdf361caf265338cd06922e1e8e04881a5f4c9
fuybooo <fuybooo@qq.com>
2017-04-08 20:57:36 +0800

    写了一天了，好累，不过使用了browser-sync，不用刷新浏览器了，太爽了，根本停不下来！

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,33 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-
-
-gulp.task('serve', function(){
-    gulp.src('./')
-        .pipe($.webserver({
-            port: '80',
-            host: '127.0.0.1',
-            liveload: true,
-            directoryListing: {
-                path: './',
-                enable: true
-            }
-        }));
-    require('opn')('http://localhost/index.html');
+var browserSync = require('browser-sync');
+gulp.task('watch', function(){
+    browserSync.init({
+        server: './'
+    });
+    
+    gulp.watch([
+        'js/**/*.js',
+        'css/**/*.css',
+        'tpls/**/*.html',
+        '*.html',
+        'img/**/*.*',
+        'json/**/*.json'
+    ]).on('change', browserSync.reload);
 });
-gulp.task('default', ['serve']);
+gulp.task('serve', function(){
+//  gulp.src('./')
+//      .pipe($.webserver({
+//          port: '80',
+//          host: '127.0.0.1',
+//          liveload: true,
+//          directoryListing: {
+//              path: './',
+//              enable: true
+//          }
+//      }));
+//  require('opn')('http://localhost/index.html');
+});
+gulp.task('default', ['watch']);
+//gulp.task('default', ['watch','serve']);
